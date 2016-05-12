bc802ab69e512718e513bfa49adbc6974a83228f
Omar Hijaz <omarhijaz@gmail.com>
2016-05-11 23:06:25 -0400

    remove google analytics

--- index.php
+++ index.php
@@ -22,14 +22,6 @@
   <script>try{Typekit.load({ async: false });}catch(e){}</script> <!-- changed to false -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <title>SILO |trainer|</title>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-75413532-1', 'auto');
-    ga('send', 'pageview');
-  </script>
 </head>
 <body>
   <div class="container">
