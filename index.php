18f46a86f7d991b78fe545ad9aba81347b5b0ea3
Omar Hijaz <omarhijaz@gmail.com>
2016-05-11 22:48:25 -0400

    major first update

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
   <meta name="description" content="Silent timeboxing, right on your wrist. Introducing a simple way to be more productive. Start getting more done in less time.">
   <meta name="keywords" content="pomodoro, timeboxing, timebox, pomodoro technique, interval timer, interval timing, silent timer">
   <meta name="apple-itunes-app" content="app-id=1086491369, affiliate-data=myAffiliateData, app-argument=myURL">
-  <link rel="apple-touch-icon" sizes="57x57" href="https://s3-us-west-2.amazonaws.com/silofocus/icon-57%402x.jpg">
-  <link rel="shortcut icon" href="https://s3-us-west-2.amazonaws.com/silofocus/favicon%401x.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="https://s3-us-west-2.amazonaws.com/silotrainer/icon-57%402x.jpg">
+  <link rel="shortcut icon" href="https://s3-us-west-2.amazonaws.com/silotrainer/favicon%401x.png">
   <!-- Latest Bootstrap compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="main.css">
@@ -21,7 +21,7 @@
   <script src="https://use.typekit.net/enq5mfv.js"></script>
   <script>try{Typekit.load({ async: false });}catch(e){}</script> <!-- changed to false -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <title>SILO |focus|</title>
+  <title>SILO |trainer|</title>
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -36,37 +36,37 @@
 
     <div class="story-block center-block" style="margin-bottom: 0px;">
       <div class="font-logo text-center">
-		    <a href="http://silofocus.com" style="text-decoration: inherit;">
-		      SILO<span style="font-weight: lighter;"> |focus|</span>
+		    <a href="http://silotrainer.com" style="text-decoration: inherit;">
+		      SILO<span style="font-weight: lighter;"> |trainer|</span>
 		    </a>
 		  </div>
       <div class="fade-in one">
 	      <div class="row center-block text-center">
-	        <h1 class="tk-myriad-pro">Time Flies, Make the Most of It</h1>
+	        <h1 class="tk-myriad-pro">Silent Intervals on your Wrist</h1>
 	      </div>
 	      <div class="row center-block visible-md-block visible-lg-block" style="margin-top: 2.2em;">
-	        <img src="https://s3-us-west-2.amazonaws.com/silofocus/watches%402x.png" alt="" class="img-responsive" onload="imgLoad(this)" style="visibility:hidden">
+	        <img src="https://s3-us-west-2.amazonaws.com/silotrainer/watches%402x.png" alt="" class="img-responsive" onload="imgLoad(this)" style="visibility:hidden">
 	      </div>
 	      <div class="row center-block visible-xs-block visible-sm-block" style="margin-top: 2.2em;">
-	        <img src="https://s3-us-west-2.amazonaws.com/silofocus/watches-sm%402x.png" alt="" class="img-responsive">
+	        <img src="https://s3-us-west-2.amazonaws.com/silotrainer/watches-sm%402x.png" alt="" class="img-responsive">
 	      </div>
 	    </div>  
       <div class="fade-in two">
 	      <div class="row text-center center-block" style="margin-top: 2em; max-width: 85%">
-	        <h2 class="tk-myriad-pro">Silo prompts you to focus–with a tap on the wrist.</h2>
+	        <h2 class="tk-myriad-pro">Silo watches the clock - so you don't have to.</h2>
 	      </div>
 	      <div class="row center-block" style="margin-top: 2.2em; max-width: 100%;">
 	        <div>
-	          <a href="https://appsto.re/us/PROWab.i" onclick="ga('send', 'event', 'App Store Top', 'Clicked', 'Spring Campaign', '1');"> <img src="https://s3-us-west-2.amazonaws.com/silofocus/App_Store_Badge_US-UK%402x.png" alt="ios" class="img-responsive center-block" style="margin-bottom: 1.2em; max-width: 35%;"> </a>
+	          <a href="https://appsto.re/us/PROWab.i" onclick="ga('send', 'event', 'App Store Top', 'Clicked', 'Spring Campaign', '1');"> <img src="https://s3-us-west-2.amazonaws.com/silotrainer/App_Store_Badge_US-UK%402x.png" alt="ios" class="img-responsive center-block" style="margin-bottom: 1.2em; max-width: 35%;"> </a>
 	        </div>
 	      </div>
 	    </div>
 	    <div class="fade-in three">
 	      <div class="row center-block visible-md-block visible-lg-block" style="margin-top: 1.2em; max-width: 80%; margin-bottom: 2em;">
-	        <img src="https://s3-us-west-2.amazonaws.com/silofocus/arrow-down%402x.png" alt="arrow" class="img-responsive center-block" style="width: 10%;">
+	        <img src="https://s3-us-west-2.amazonaws.com/silotrainer/arrow-down%402x.png" alt="arrow" class="img-responsive center-block" style="width: 10%;">
 	      </div>
 	      <div class="row center-block visible-xs-block visible-sm-block" style="margin-top: 1.8em; max-width: 80%; margin-bottom: 0em;">
-	        <img src="https://s3-us-west-2.amazonaws.com/silofocus/arrow-down%402x.png" alt="arrow" class="img-responsive center-block" style="width: 10%;">
+	        <img src="https://s3-us-west-2.amazonaws.com/silotrainer/arrow-down%402x.png" alt="arrow" class="img-responsive center-block" style="width: 10%;">
 	      </div>
 	    </div>
     </div> 
@@ -75,137 +75,86 @@
       
     	<div class="story-block center-block hideme">
         <div class="row center-block text-center">
-          <h1 class="tk-myriad-pro">More Done, in Less Time</h1>
+          <h1 class="tk-myriad-pro">The Power of Touch and Time</h1>
         </div>
-        <div class="row center-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/timer%402x.png" alt="timer" class="img-responsive center-block" style="max-width: 50%">
+        <div class="row center-block" style="margin-top: 2.4em;">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/haptics%402x.png" alt="haptics" class="img-responsive center-block" style="max-width: 50%">
         </div>
         <div class="row text-center" style="margin-top: 2.2em;">
-          <p>The act of focusing on a task uninterrupted, followed by a short break, is a sure way to boost productivity. Silo Focus manages your time, so you don't have to.</p>
+          <p>Make your workout more effective with haptic cues between exercise and recovery intervals. Using the Watch Taptic Engine, Silo gives you sharp sense of time and the motivation to get it done.</p>
         </div>
       </div>
 
       <div class="story-block center-block hideme">
       	<div class="row center-block visible-md-block visible-lg-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-settings%402x.png" alt="have fun" class="img-responsive center-block">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/box-settings%402x.png" alt="have fun" class="img-responsive center-block">
         </div>
         <div class="row center-block visible-xs-block visible-sm-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-settings-sm%402x.png" alt="have fun" class="img-responsive center-block">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/box-settings-sm%402x.png" alt="have fun" class="img-responsive center-block">
         </div>
        </div>
 
       <div class="story-block center-block hideme">
         <div class="row center-block text-center">
-          <h1 class="tk-myriad-pro">Silence Speaks Volumes</h1>
+          <h1 class="tk-myriad-pro">Set it + Forget It</h1>
         </div>
-        <div class="row center-block" style="margin-top: 2.4em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/haptics%402x.png" alt="haptics" class="img-responsive center-block" style="max-width: 50%">
+        <div class="row center-block" style="margin-top: 2.2em;">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/timer%402x.png" alt="timer" class="img-responsive center-block" style="max-width: 50%">
         </div>
         <div class="row text-center margin-bottom" style="margin-top: 2.4em;">
-          <p>Silo uses the Taptic Engine on Watch, to nudge you between focus and break cycles. This gives you a new sense of time and motivation to get work done.</p>
+          <p>The simple interface lets you set multiple different interval timers and repetitions. When you’re ready to start, simply raise your wrist and press “GO”.</p>
         </div>
       </div>
 
       <div class="story-block center-block hideme">
       	<div class="row center-block visible-md-block visible-lg-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-activities%402x.png" alt="have fun" class="img-responsive center-block">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/box-activities%402x.png" alt="have fun" class="img-responsive center-block">
         </div>
         <div class="row center-block visible-xs-block visible-sm-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-activities-sm%402x.png" alt="have fun" class="img-responsive center-block">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/box-activities-sm%402x.png" alt="have fun" class="img-responsive center-block">
+        </div>
+      </div>
+
+      <div class="story-block center-block hideme">
+        <div class="row">
+          <div class="col-sm-4 col-xs-4">
+            <img src="https://s3-us-west-2.amazonaws.com/silotrainer/watch-heart%402x.png" alt="watch done" class="img-responsive center-block" style="max-width: 100%">
+          </div>
+          <div class="col-sm-8 col-xs-8">
+            <h1 class="tk-myriad-pro" style="max-width: 80%;">Stay Ahead of the Game—At your Own Pace</h1>
+          </div>
+        </div>
+        <div class="row text-center" style="margin-top: 2.2em;">
+          <p>At the end of a workout, Silo commends you and gives you a glance at your heart rate. This helps you know your limit and keep track of your results over time.</p>
         </div>
       </div>
 
       <div class="story-block center-block hideme">
         <div class="row center-block text-center">
-          <h1 class="tk-myriad-pro">Productive Work and Study</h1>
+          <h1 class="tk-myriad-pro">HIIT - Tabata - Crossfit</h1>
         </div>
         <div class="row center-block" style="margin-top: 1.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/working%402x.png" alt="working" class="img-responsive center-block" style="max-width: 25%">
+          <img src="https://s3-us-west-2.amazonaws.com/silotrainer/working%402x.png" alt="working" class="img-responsive center-block" style="max-width: 25%">
         </div>
         <div class="row text-center" style="margin-top: 2.2em;">
-          <p>Designers and developers use this timeboxing technique to enhance efficiency and creative output. Unlike distracting timers/apps, Silo is subtle, yet powerful.</p>
-        </div>
-      
-        <div class="row center-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/graduate%402x.png" alt="graduate" class="img-responsive center-block" style="max-width: 25%">
-        </div>
-        <div class="row text-center" style="margin-top: 2.2em;">
-          <p>While studying or writing, Silo gives you a diciplined reminder–right on your wrist. It’s discreet and silent, allowing you to work without distracting anyone nearby.</p>
-        </div>
-      </div>
-
-      <div class="story-block center-block hideme">
-      	<div class="row center-block visible-md-block visible-lg-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-office%402x.png" alt="have fun" class="img-responsive center-block">
-        </div>
-        <div class="row center-block visible-xs-block visible-sm-block" style="margin-top: 2.2em;">
-          <img src="https://s3-us-west-2.amazonaws.com/silofocus/box-office-sm%402x.png" alt="have fun" class="img-responsive center-block">
+          <p>Designed for variety workouts that burn fat and build muscle, Silo lets you set timers across a number of different fitness modes: cardio intervals, strength training, as well as gymnastics movements.</p>
         </div>
       </div>
 
       <div class="story-block center-block hideme">
         <div class="row">
           <div class="col-sm-4 col-xs-4">
-            <img src="https://s3-us-west-2.amazonaws.com/silofocus/watch-break%402x.png" alt="watch done" class="img-responsive center-block">
+            <img src="https://s3-us-west-2.amazonaws.com/silotrainer/watch-done%402x.png" alt="watch done" class="img-responsive center-block" style="max-width: 100%">
           </div>
           <div class="col-sm-8 col-xs-8">
-            <h1 class="tk-myriad-pro">The Short Break</h1>
-            <h3>
-              <ul>
-                <li style="margin-top: .6em;">Go for a walk</li>
-                <li style="margin-top: .3em;">Unplug, rest your eyes</li>
-                <li style="margin-top: .3em;">Check email</li>
-                <li style="margin-top: .3em;">Stand up and stretch</li>
-                <li style="margin-top: .3em;">Organize workspace</li>
-                <li style="margin-top: .3em;">Prepare the next task</li>
-              </ul>
-            </h3>
-          </div>
-        </div>
-        <div class="row text-center" style="margin-top: 2.2em;">
-          <p>The break is vital to letting your mind recover and refresh after intense focus. At the same time, it triggers a reminder for your body to keep moving.</p>
-        </div>
-      </div>
-
-      <div id="container">
-      <div class="story-block center-block hideme background-wave">
-        <div class="row center-block text-center">
-          <h1>Find your rhythm</h1>
-        </div>
-        <div class="row text-left center-block" style="margin-top: 1.2em; max-width: 100%">
-          <h4>25|5 Classic</h4>
-          <p>
-            The traditional way of getting stuff done. Focus on a task for 25 minutes then take a 5-minute break. Repeat 4 times. 
-          </p>
-        </div>
-        <div class="row text-left center-block" style="margin-top: 1.2em; max-width: 100%">
-          <h4>20|10 Moderate</h4>
-          <p>
-            This is a less taxing way to work. Focus for 20 minutes then take a 10-minute break. Repeat 4 times.
-          </p>
-        </div>
-        <div class="row text-left center-block" style="margin-top: 1.2em; max-width: 100%">
-          <h4>45|15 Power</h4>
-          <p>
-            Sometimes you have to power through a lot of work. Focus for 45 minutes the take a 15-minute break. Repeat 3 times.
-          </p>
-        </div>
-      </div>
-
-      <div class="story-block center-block hideme">
-        <div class="row">
-          <div class="col-sm-4 col-xs-4">
-            <img src="https://s3-us-west-2.amazonaws.com/silofocus/watch-done%402x.png" alt="watch done" class="img-responsive center-block" style="max-width: 100%">
-          </div>
-          <div class="col-sm-8 col-xs-8">
-            <h1 class="tk-myriad-pro" style="max-width: 80%;">It’s about time to let time manage itself.</h1>
+            <h1 class="tk-myriad-pro" style="max-width: 80%;">The Most Personal Trainer, Right on your Wrist.</h1>
           	<div style="max-width: 60%;">
-		          <a href="https://appsto.re/us/PROWab.i" onclick="ga('send', 'event', 'App Store Bottom', 'Clicked', 'Spring Campaign', '1');"> <img src="https://s3-us-west-2.amazonaws.com/silofocus/App_Store_Badge_US-UK%402x.png" alt="ios" class="img-responsive center-block" style="margin-top: 1.2em;"> </a>
+		          <a href="https://appsto.re/us/PROWab.i" onclick="ga('send', 'event', 'App Store Bottom', 'Clicked', 'Spring Campaign', '1');"> <img src="https://s3-us-west-2.amazonaws.com/silotrainer/App_Store_Badge_US-UK%402x.png" alt="ios" class="img-responsive center-block" style="margin-top: 1.2em;"> </a>
 		        </div>
           </div>
         </div>
         <div class="row text-center" style="margin-top: 2.2em;">
-          <p>By avoiding interruptions that chew up time, Silo lets you end your day feeling more productive. This lets you accomplish goals on target, and have time to spare. </p>
+          <p>Silo Trainer watches the clock, so you don’t have to. A subtle tap on the wrist is all you need to stay on target and end your workout feeling more productive.</p>
         </div>
       </div>
 
@@ -224,21 +173,21 @@
       
       <div class="row center-block hideme" style="margin-top: 2.2em; max-width: 90%;">
         <h2 class="col-sm-4 col-xs-4 text-center">
-          <a href="http://silofocus.com/mission" onclick="ga('send', 'event', 'Mission', 'Clicked', 'Spring Campaign', '1');"><img src="https://s3-us-west-2.amazonaws.com/silofocus/mission%402x.png" alt="Mission" title="Mission" class="" style="width: 30%; min-width: 50px;"></a>
+          <a href="http://silotrainer.com/mission" onclick="ga('send', 'event', 'Mission', 'Clicked', 'Spring Campaign', '1');"><img src="https://s3-us-west-2.amazonaws.com/silotrainer/mission%402x.png" alt="Mission" title="Mission" class="" style="width: 30%; min-width: 50px;"></a>
         </h2>
         <h3 class="col-sm-4 col-xs-4" >
           <a href="#" class="scrollup">
-            <img src="https://s3-us-west-2.amazonaws.com/silofocus/arrow-down%402x.png" alt="arrow" class="img-responsive center-block rotate180" style="width: 20%;">
+            <img src="https://s3-us-west-2.amazonaws.com/silotrainer/arrow-down%402x.png" alt="arrow" class="img-responsive center-block rotate180" style="width: 20%;">
           </a>
         </h3>
         <h2 class="col-sm-4 col-xs-4 text-center">
-          <a href="http://silofocus.com/contact" class="text-center" onclick="ga('send', 'event', 'Contact', 'Clicked', 'Spring Campaign', '1');"><img src="https://s3-us-west-2.amazonaws.com/silofocus/contact%402x.png" alt="Contact" title="Contact" class="" style="width: 30%; min-width: 50px;"></a>
+          <a href="http://silotrainer.com/contact" class="text-center" onclick="ga('send', 'event', 'Contact', 'Clicked', 'Spring Campaign', '1');"><img src="https://s3-us-west-2.amazonaws.com/silotrainer/contact%402x.png" alt="Contact" title="Contact" class="" style="width: 30%; min-width: 50px;"></a>
         </h2>        
       </div>
     </div>
     <div class="row center-block border-footer" style="margin-top: 2.2em; max-width: 80%;">
       <footer style="color: #8087A6; margin-top: 0.5em;">
-        Silo |focus| &copy 2016 Dark Matter Developers
+        Silo |trainer| &copy 2016 Dark Matter Developers
       </footer>
     </div>
     <script type="text/javascript"> /* Fading divs */
